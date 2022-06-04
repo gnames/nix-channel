945d763f3b8ccbc604ad7364f377ed00a89c9651
Dmitry Mozzherin <dmozzherin@gmail.com>
2022-06-04 17:37:16 -0500

    wip

--- default.nix
+++ default.nix
@@ -1,14 +1,8 @@
 { pkgs ? import <nixpkgs> { } }:
 
 rec {
-  home-manager = pkgs.callPackage ./home-manager { path = toString ./.; };
-
-  install = with pkgs; {
-    myriatrix-20-05 = callPackage ./gn-sources/myriatrix/20-05;
-    myriatrix-21-02 = callPackage ./gn-sources/myriatrix/21-02;
-    myriatrix-21-07 = callPackage ./gn-sources/myriatrix/21-07;
-    myriatrix = myriatrix-21-07;
-
-    path = ./.;
-  };
+  myriatrix-20-05 = callPackage ./gn-sources/myriatrix/20-05;
+  myriatrix-21-02 = callPackage ./gn-sources/myriatrix/21-02;
+  myriatrix-21-07 = callPackage ./gn-sources/myriatrix/21-07;
+  myriatrix = myriatrix-21-07;
 }
--- flake.nix
+++ flake.nix
@@ -2,7 +2,17 @@
 # See https://github.com/NixOS/rfcs/pull/49 for details.
 {
   description = "A collection of packages for gnames";
+  inputs = {
+    nixpkgs.url = github:NixOS/nixpkgs;
+  };
 
-  outputs = { self }:
-  {}
+  outputs = { self, nixpkgs }:
+  let
+   system = "x86_64-linux";
+   pkgs = nixpkgs.legacyPackages.${system};
+
+  in
+  with pkgs; {
+    myriatrix = callPackage ./gn-sources/myriatrix/21-07;
+  };
 }
