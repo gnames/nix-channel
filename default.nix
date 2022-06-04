93d2e80c9ee53ab5b61cc87ee0162d81bd6824f2
Dmitry Mozzherin <dmozzherin@gmail.com>
2022-06-04 17:20:56 -0500

    wip

--- default.nix
+++ default.nix
@@ -7,6 +7,7 @@ rec {
     myriatrix-20-05 = callPackage ./gn-sources/myriatrix/20-05;
     myriatrix-21-02 = callPackage ./gn-sources/myriatrix/21-02;
     myriatrix-21-07 = callPackage ./gn-sources/myriatrix/21-07;
+    myriatrix = myriatrix-21-07;
 
     path = ./.;
   };
---
+++ flake.nix
@@ -0,0 +1,8 @@
+# Experimental flake interface to Nixpkgs.
+# See https://github.com/NixOS/rfcs/pull/49 for details.
+{
+  description = "A collection of packages for gnames";
+
+  outputs = { self }:
+  {}
+}
