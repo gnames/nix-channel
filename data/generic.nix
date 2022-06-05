29a21da1c687929a1b089cca00cf6b079e956773
Dmitry Mozzherin <dmozzherin@gmail.com>
2022-06-05 12:24:11 -0500

    wip

---
+++ data/generic.nix
@@ -0,0 +1,30 @@
+{ lib, stdenv, fetchurl, pname, version, description, homepage }:
+
+let
+data = import ./sources.nix;
+dir = "share/data";
+
+in
+stdenv.mkDerivation rec {
+  inherit pname version;
+  name = pname + "-" + version;
+
+  src = fetchurl {
+    url = data.url + data.${name}.file;
+    sha256 = data.${name}.sha256;
+  };
+
+  unpackPhase = "true";
+
+  installPhase = ''
+    mkdir -p $out/${dir}
+
+    cp $src $out/${dir}/${pname}.tar.gz
+  '';
+
+  meta = with lib; {
+    inherit description homepage;
+    license = licenses.cc0;
+    maintainers = with maintainers; [ "dimus" ];
+  };
+}
---
+++ data/irmng/default.nix
@@ -0,0 +1,44 @@
+{ lib, stdenv, fetchurl }:
+
+let
+generic = import ../generic.nix;
+pname = "irmng";
+description = "IRMNG";
+homepage = "https://www.gbif.org/dataset/d7dddbf4-2cf0-4f39-9b2a-bb099caae36c";
+
+in {
+ gn-gbif_15_07 = generic {
+   inherit pname description homepage lib stdenv fetchurl;
+   version = "15-07";
+ };
+
+ gn-gbif_17_02 = generic {
+   inherit pname description homepage lib stdenv fetchurl;
+   version = "17-02";
+ };
+
+ gn-gbif_19_09 = generic {
+   inherit pname description homepage lib stdenv fetchurl;
+   version = "19-09";
+ };
+
+ gn-gbif_20_05 = generic {
+   inherit pname description homepage lib stdenv fetchurl;
+   version = "20-05";
+ };
+
+ gn-gbif_21_01 = generic {
+   inherit pname description homepage lib stdenv fetchurl;
+   version = "21-01";
+ };
+
+ gn-gbif_21_06 = generic {
+   inherit pname description homepage lib stdenv fetchurl;
+   version = "21-06";
+ };
+
+ gn-gbif_21_12 = generic {
+   inherit pname description homepage lib stdenv fetchurl;
+   version = "21-12";
+ };
+}
