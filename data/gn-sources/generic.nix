d38feca507da44d23fa5d67b9a90a8f7c9a579e6
Dmitry Mozzherin <dmozzherin@gmail.com>
2022-06-09 05:58:47 -0500

    wip

--- data/gn-sources/generic.nix
+++ data/gn-sources/generic.nix
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
   name = pname + "-" + version;
 
   src = fetchurl {
-    url = data.url + data.${name}.file;
+    url = data.gn.url + data.${name}.file;
     sha256 = data.${name}.sha256;
   };
 
